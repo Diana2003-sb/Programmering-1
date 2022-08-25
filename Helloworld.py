a5f7269d148b818e60dc071abf25e41a6504f20a
Diana Sabetbashloghi <diana.sabetbashloghil@ga.ntig.se>
2022-08-25 09:46:35 +0200

    Upd

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,14 @@
-print("diana är bra")
+#Variabler 
+from ast import Name
+
+
+Username="Diana"
+age= 19
+print(Username)
+name=input("Hej, vad heter du?")
+print("Vällkomen",name ,"till min program")
+age=input(" Hur gmmalr är du?")
+color=input("vilken färg gillar du?")
+food=input("vad är din favoritmat?")
+input("press enter to continue")
+print("du heter", name,"\ndin favoritmat är", food, "\noch din favoritfärg är",color) 
