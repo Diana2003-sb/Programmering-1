8a7c5164d45f4008bebb9c9430dadc221a45a325
Diana Sabetbashloghi <diana.sabetbashloghil@ga.ntig.se>
2022-08-29 12:55:03 +0200

    up

---
+++ lektion2.py
@@ -0,0 +1,23 @@
+from re import A
+
+
+age=19 
+months=228
+second=599184000
+age=input("hur gammal är du?")
+print(age,"år","är lika med", months,"månader","och",second, "sekunder")
+
+a=12
+b=2
+print(7-8)
+print(a+b)
+ab=12+2
+print(a/b)
+ab=12/2
+
+print(3**2)
+e=4
+b=2
+print(e**b)
+eb=4**2
+print(8-6)
